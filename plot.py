7816bf24f8b1bddbc1e0ea08e90591d4976c8b25
Ali Nazari <ali.nazari@aut.ac.ir>
2019-01-27 11:09:52 +0330

    refactor clean_data function

--- plot.py
+++ plot.py
@@ -3,25 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def clean_data(df):
+
+def clean_data(data_frame):
     to_delete = []
-    for index, row in df.iterrows():
-        if  math.isnan(row['Price']) or math.isnan(row['FROM']) or math.isnan(row['TO']) or math.isnan(row['Departure_Time']):
+    for index, row in data_frame.iterrows():
+        if math.isnan(row['Price']) or math.isnan(row['FROM']) or math.isnan(row['TO']) or math.isnan(
+                row['Departure_Time']):
             print(row)
             # print(index)
             to_delete.append(index)
 
     print(len(to_delete))
     # print(to_delete)
-    df = df.drop(to_delete)
-        
-    df.to_csv('cleaned.csv',index=False)
-    return df
+    data_frame = data_frame.drop(to_delete)
+
+    data_frame.to_csv('cleaned.csv', index=False)
+    return data_frame
+
 
 df = pd.read_csv('data.csv')
 print(df.head())
 print(df.describe())
 df = clean_data(df)
-
-
-
